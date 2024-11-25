a797b67c969c30eef88ab4e41d98a8a2e8a8d6cc
freitasdev <frtecnologiadigital@gmail.com>
2024-11-25 08:42:13 -0300

    pronto

--- app/View/Components/educacionalLayout.php
+++ app/View/Components/educacionalLayout.php
@@ -597,7 +597,7 @@ class educacionalLayout extends Component
     public function render(): View|Closure|string
     {
         $Tipo = Auth::user()->tipo;
-        return view('components.educacional-layout',[
+        return view('components.Educacional-layout',[
             "Modulos" => $this->modulos
         ]);
     }
